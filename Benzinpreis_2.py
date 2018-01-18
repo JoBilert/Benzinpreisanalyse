c1bf8690d6cad0b2ff2d6eeba3f2ac0d31bbd4a9
TalmarGrosskotz <bilert@gmail.com>
2018-01-18 10:51:31 +0100

    update

--- Benzinpreis_2.py
+++ Benzinpreis_2.py
@@ -43,21 +43,52 @@ stations = []
 
 #us parameters to generate the web-adress
 source = 'http://www.clever-tanken.de/tankstelle_liste?spritsorte='+fuel_type+'&ort='+plz+'+'+town+'&r='+radius+'&sort=km'
-print(source)
+
 #loads the url into the parser
 def load_page(url):
     page = requests.get(url)
     content = BeautifulSoup(page.text, 'html.parser')
     return content
 
-#extract stations and prices and wrtie them into Station_Objs
-def get_data(content):
+#extract price-table from webpage
+def get_stations(content):
     sta = content.find_all(class_='price-entry')
-    print(sta)
-    return sta
+    for station in sta:
+        station_address = get_address(station)
+        stations.append(Station(station_address)) 
+
+#extrace address from price-table
+def get_address(raw):
+    #get address-info in html
+    ad1 = raw.find(class_='row fuel-station-location-name')
+    ad2 = raw.find(id = 'fuel-station-location-street')
+    ad3 = raw.find(id = 'fuel-station-location-city')
     
+    #strip html-tags
+    ad1_str = ad1.contents[0]
+    ad2_str = ad2.contents[0]
+    ad3_str = ad3.contents[0]
+    #combine into address-string
+    address = ad1_str +'\n'+ ad2_str +'\n'+ ad3_str
+    return address
+
+#adds prices to the station_object
+def get_price(content):
+    sta = content.find_all(class_='price-entry')
+    for station in stations:
+        station.append(extract_price(sta))
+        
+#extract prices from html
+def extract_price(raw):
+    price_temp = raw.find(class_='price')
+    print (price_temp)
+    price = float(price_temp.contents[0])
+    #print (price)
+    return price
+    
+
 test = load_page(source)
-test2 = get_data(test)
-print(test2)
-
-
+test2= get_stations(test)
+get_price(test)
+print(stations[1].address)
+print(stations[1].prices)
